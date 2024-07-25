e3aa833c1a51c994577ae14298c654d25778b9a3
Dragos Ursu <dragos.ursu15@gmail.com>
2024-07-25 11:51:25 +0300

    project init

---
+++ src/main/java/com/bearsoft/pharmadepot/PharmaDepotApplication.java
@@ -0,0 +1,13 @@
+package com.bearsoft.pharmadepot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaDepotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmaDepotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bearsoft/pharmadepot/PharmaDepotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bearsoft.pharmadepot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaDepotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
